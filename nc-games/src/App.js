d455227fca2d4307575efabb0a05662662071779
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-09 14:54:35 +0000

    removed cors from frontend

--- nc-games/src/App.js
+++ nc-games/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-const cors = require('cors');
-
-App.use(cors());
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ nc-games/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export function Header() {
+    return <h1 className="header">Northen Board Gamers</h1>;
+  }
