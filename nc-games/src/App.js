9f6427f47778d8ca80821193072a66c87f8f1559
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-09 15:30:30 +0000

    added basic navbar

--- nc-games/src/App.js
+++ nc-games/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
---
+++ nc-games/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+return (
+    <nav>
+    <Link to='/'>Home</Link>
+    <span> | </span>
+    <Link to='/reviews'>Reviews</Link>
+    <span> | </span>
+    <Link to='/strategy'>Strategy</Link>
+    <span> | </span>
+    <Link to='/hidden-roles'>Hidden Roles</Link>
+    <span> | </span>
+    <Link to='/dexterity'>Dexterity</Link>
+    <span> | </span>
+    <Link to='/push-your-luck'>Push Your Luck</Link>
+    <span> | </span>
+    <Link to='/roll-and-write'>Roll And Write</Link>
+    <span> | </span>
+    <Link to='/deck-building'>Deck Building</Link>
+    <span> | </span>
+    <Link to='/engine-building'>Engine Building</Link>
+    <p>Hello There!</p>
+    <Link to='/login'>Login Here!</Link>
+    </nav>
+)
+};
+
+export default Navbar;
