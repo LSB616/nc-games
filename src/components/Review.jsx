09ba5dd7c3ea4f4b1a3894d38fb80aed7df48f61
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-10 15:09:53 +0000

    added comments to individual reviews

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getReview } from "../utils/api";
+import Comments from "./Comments"
 
 const Review = () => {
 const [isLoading, setIsLoading] = useState(true);
@@ -29,8 +30,9 @@ return (
     <p>Designer: {review.designer}</p>  
     <p>Reviewer: {review.owner}</p>
     </div>
-    <h3 className="review_votes">Votes: {review.votes}</h3>
+    <h3 className="review_votes">Likes: {review.votes}</h3>
     <p className="review_body">{review.review_body}</p>
+    <Comments review_id={review_id}/>
     </div>
 )
 };
