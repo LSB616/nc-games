690a6f08c9fe695a3e2287fdbf7c1a390b5f2896
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-12 15:40:44 +0000

    sortby and order dropdowns created and implemented

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,43 +5,50 @@ import { Link, useSearchParams } from 'react-router-dom';
 const Reviews = () => {
 const [isLoading, setIsLoading] = useState(true);
 const [reviews, setReviews] = useState([]);
-const [sortByParams, setSortByParams] = useSearchParams('');
-const sortbyQuery = sortByParams.get('sort_by');
-const [sortBy, setSortBy] = useState('category')
-const [orderBy, setOrderBy] = useState(null)
-
+const [searchParams, setSearchParams] = useSearchParams();
+const sortbyQuery = searchParams.get('sort_by');
+const orderQuery = searchParams.get('order');
 
 
 useEffect(() => {
     setIsLoading(true);
-    getReviews(sortbyQuery).then(({ reviews }) => {
+    getReviews(sortbyQuery, orderQuery).then(({ reviews }) => {
         setReviews(reviews);
         setIsLoading(false);
     });
-}, [sortBy]);
+}, [sortbyQuery, orderQuery]);
 
 if (isLoading) {
     return <p className="Loading">Loading...</p>;
 };
 
+const setSearch = (sort_by, order) => {
+    const newParams = new URLSearchParams(searchParams);
+    if (sort_by) {newParams.set('sort_by', sort_by)};
+    if(order) {newParams.set('order', order)};
+    setSearchParams(newParams);
+  };
+
+
 return (
 <div>
 <section>
-    <select className="sortby-dropdown" value={sortByParams} onChange={(e) => {
-        setSortBy(e.target.value.split('=')[1])
-        setSortByParams(e.target.value)
+    <select className="sortby-dropdown" value={sortbyQuery} onChange={(e) => {
+        setSearch(e.target.value, null)
         }}>
-    <option value="sort_by=title">Title</option>
-    <option value="sort_by=owner">Reviewer</option>
-    <option value="sort_by=category">Category</option>
-    <option value="sort_by=designer">Designer</option>
-    <option value="sort_by=created_at">Date</option>
+    <option disabled="disabled" selected={true} hidden={true}>Sort By</option>        
+    <option value="title">Title</option>
+    <option value="owner">Reviewer</option>
+    <option value="category">Category</option>
+    <option value="designer">Designer</option>
+    <option value="created_at">Date</option>
     </select>
-    <select className="orderby-dropdown" value={orderBy} onChange={(e) => {
-        setOrderBy(e.target.value)
+    <select className="orderby-dropdown" value={orderQuery} onChange={(e) => {
+        setSearch(null, e.target.value)
     }}>
-    <option value="order=asc">Ascending</option>
-    <option value="order=desc">Descending</option>
+    <option disabled="disabled" selected={true} hidden={true}>Order</option> 
+    <option value="asc">Ascending</option>
+    <option value="desc">Descending</option>
     </select>
 </section>
 <section class="cards-wrapper">
