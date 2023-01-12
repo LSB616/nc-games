cd19c792114cd84ba86d7d8dbf11f3c42a1dfba2
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-12 11:02:30 +0000

    working towards sort_by queries working

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
 import { getReviews } from "../utils/api";
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Reviews = () => {
 const [isLoading, setIsLoading] = useState(true);
 const [reviews, setReviews] = useState([]);
-const [sortBy, setSortBy] = useState('');
+const [sortByParams, setSortByParams] = useSearchParams('');
+const sortbyQuery = sortByParams.get('sort_by');
+
+console.log(sortbyQuery)
 
 useEffect(() => {
     setIsLoading(true);
-    getReviews(sortBy).then(({ reviews }) => {
+    getReviews(sortByParams).then(({ reviews }) => {
         setReviews(reviews);
         setIsLoading(false);
     });
-}, [sortBy]);
+}, [sortByParams]);
 
 if (isLoading) {
     return <p className="Loading">Loading...</p>;
@@ -22,26 +25,26 @@ if (isLoading) {
 return (
 <section class="cards-wrapper">
 <section>
-    <select className="sortby-dropdown" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-    <option value="title">Title</option>
-    <option value="owner">Reviewer</option>
-    <option value="category">Category</option>
-    <option value="designer">Designer</option>
-    <option value="votes">Likes</option>
-    <option value="created_at">Date</option>
-    <option value="comment_count">Comment Count</option>
+    <select className="sortby-dropdown" value={sortByParams} onChange={(e) => setSortByParams(e.target.value)}>
+    <option value="sort_by=title">Title</option>
+    <option value="sort_by=owner">Reviewer</option>
+    <option value="sort_by=category">Category</option>
+    <option value="sort_by=designer">Designer</option>
+    <option value="sort_by=votes">Likes</option>
+    <option value="sort_by=created_at">Date</option>
+    <option value="sort_by=comment_count">Comment Count</option>
     </select>
 </section>
 {reviews.map((review) => { 
-return  <div class="card-grid-space" key={review.review_id}>
+return  <div className="card-grid-space" key={review.review_id}>
         <Link to={`/reviews/${review.review_id}`} className="card-link">
-        <a class="card" style={{"--bg-img": `url(${review.review_img_url})`}}>
+        <a className="card" style={{"--bg-img": `url(${review.review_img_url})`}}>
         <div>
         <h1>{review.title}</h1>
         <p>{review.owner}</p>
-        <div class="date">{review.created_at.slice(0, 10)}</div>
-        <div class="tags">
-        <div class="tag">{review.category}</div>
+        <div className="date">{review.created_at.slice(0, 10)}</div>
+        <div className="tags">
+        <div className="tag">{review.category}</div>
         </div>
         </div>
         </a>
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,8 @@ const boardGamesApi = axios.create({
     baseURL: "https://boardgame-reviews.onrender.com/api"
 });
 
-export const getReviews = (sortBy) => {
-    return boardGamesApi.get("/reviews", { params: { sortBy: sortBy } }).then((res) => {
+export const getReviews = (sortByParams) => {
+    return boardGamesApi.get("/reviews", { params: { sortBy: sortByParams } }).then((res) => {
         return res.data;
     });
 };
