85d37c8b2e81ee94795493f48ed24c63cd28996e
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-12 14:13:27 +0000

    category page created with working links

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -1,12 +1,43 @@
+import { Link } from 'react-router-dom'
+
 const CategoryPage = () => {
 return (
     <div className="col">
+        <Link to='/reviews?category=strategy'>
         <div className="homepage-card no1">
             <h4>Strategy</h4>
         </div>
+        </Link>
+        <Link to='/reviews?category=hidden-roles'>
         <div className="homepage-card no2">
+            <h4>Hidden Roles</h4>
+        </div>
+        </Link>
+        <Link to='/reviews?category=dexterity'>
+        <div className="homepage-card no1">
             <h4>Dexterity</h4>
         </div>
+        </Link>
+        <Link to='/reviews?category=push-your-luck'>
+        <div className="homepage-card no2">
+            <h4>Push Your Luck</h4>
+        </div>
+        </Link>
+        <Link to='/reviews?category=roll-and-write'>
+        <div className="homepage-card no1">
+            <h4>Roll And Write</h4>
+        </div>
+        </Link>
+        <Link to='/reviews?category=deck-building'>
+        <div className="homepage-card no2">
+            <h4>Deck Building</h4>
+        </div>
+        </Link>
+        <Link to='/reviews?category=engine-building'>
+        <div className="homepage-card no1">
+            <h4>Engine Building</h4>
+        </div>
+        </Link>
     </div>
 )
 };
