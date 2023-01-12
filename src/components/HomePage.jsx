dc2edb2dda1a932adbd917874e64bfeb34652793
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-12 03:47:51 +0000

    added CSS styling to the navbar and homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,24 @@
 export function HomePage () {
     return (
-        <div>
-            <img src="/home/lewis/Northcoders/Frontend/fe-project/nc-games/nc-games/boardgame-banner.png" alt="boardgame pieces"></img>
-            <h2>Welcome To Northen Board Gamers!</h2> 
-            <p>Thanks for dropping in! Northen Board Gamers is the place to find honest to the point reviews on all the most recent and classic Board Games, Card games, and Tabletop Games. The reviews are informative and the community is welcoming. So, if you like what you see, create an account and get involved! Or feel free to take a little look around ...</p>
-            <a href="https://boardgame-reviews.onrender.com/api/reviews">Reviews</a>
-            <a href="https://boardgame-reviews.onrender.com/api/categories">Categories</a>
+        <div className="homepage-container">
+            <div className="row">
+            <div className="col">
+            <h1 className="title">Northern Board Gamers</h1>
+            <h2 className="subtitle">Welcome To Northern Board Gamers!</h2> 
+            <p className="blurb">Thanks for dropping in! Northen Board Gamers is the place to find honest to the point reviews on all the most recent and classic Board Games, Card games, and Tabletop Games. The reviews are informative and the community is welcoming. So, if you like what you see, create an account and get involved! Or feel free to take a little look around ...</p>
+            </div>
+            <div className="col">
+                <div className="homepage-card no1">
+                    <h4>Reviews</h4>
+                </div>
+                <div className="homepage-card no2">
+                    <h4>Categories</h4>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
+
+{/* <a href="https://boardgame-reviews.onrender.com/api/reviews">Reviews</a>
+<a href="https://boardgame-reviews.onrender.com/api/categories">Categories</a> */}
