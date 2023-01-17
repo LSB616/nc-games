683cbaffd82ff3f3fa0dc6236d9ad613f5ae9b3d
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-17 20:09:00 +0000

    added delete review functionality but issue with looking at reviews while not logged in

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getReview } from "../utils/api";
+import { useContext, useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getReview, deleteReview } from "../utils/api";
+import { UserContext } from '../contexts/User';
 import Votes from "./Votes";
 import Comments from "./Comments"
 import LoadingPage from "./LoadingPage";
+import Reviews from "./Reviews";
 
 
 const Review = () => {
-const [isLoading, setIsLoading] = useState(true);
+const { user } = useContext(UserContext);
+const [isLoading, setIsLoading] = useState();
+const [reviewDeleted, setReviewDeleted] = useState();
 const [review, setReview] = useState('');
-const { review_id } = useParams()
+const { review_id } = useParams();
 
 
 
@@ -18,13 +22,56 @@ useEffect(() => {
     getReview(review_id).then(({ review }) => {
         setReview(review);
         setIsLoading(false);
+        setReviewDeleted(false);
     });
 }, [review_id]);
 
 
+const deleteContent = (e) => {
+    e.preventDefault();
+    return deleteReview(review_id)
+    .then(() => {
+        setReviewDeleted(true);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+};
+
 if (isLoading) {
     return <LoadingPage />;
 };
+
+if (reviewDeleted) {
+    return <Reviews />;
+};
+
+
+if (user.username === review.owner) {
+    return (
+        <section className="review">
+        <div className="review-top">
+        <h2 className="review_title">{review.title}</h2>
+        <div className="review_p">
+        <p>Category: {review.category}</p>
+        <p>Designer: {review.designer}</p>  
+        <p>Reviewer: {review.owner}</p>
+        </div>
+        <button className="delete-review-button" onClick={deleteContent}>Delete Review</button>
+        <div className="review_votes">
+        <Votes votes ={review.votes} review_id={review_id} />
+        </div>
+        </div>
+        <div className="review-bottom">
+        <p className="review_body">{review.review_body}</p>
+        <img src={`${review.review_img_url}`} className="review_img" alt="Image Associated with Board Game"></img>
+        <div className="comments">
+        <Comments review_id={review_id} />
+        </div>
+        </div>
+        </section>
+    )
+}
 
 return (
     <section className="review">
