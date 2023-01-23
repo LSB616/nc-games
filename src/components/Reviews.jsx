ddd5d4c81f8b95681df5403d54b24b8f88f0c6a4
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-01-23 02:35:45 +0000

    changed reviews to be hexagons

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -59,23 +59,26 @@ return (
     <option value="desc">Descending</option>
     </select>
 </section>
-<section className="cards-wrapper">
+<section >
+<div class="grid">
+<ul id="hexGrid">
 {reviews.map((review) => { 
-return  <div className="card-grid-space" key={review.review_id}>
-        <Link to={`/reviews/${review.review_id}`} className="card-link">
-        <div className="card" style={{"--bg-img": `url(${review.review_img_url})`}}>
-        <div>
-        <h1>{review.title}</h1>
-        <p>{review.owner}</p>
-        <div className="date">{review.created_at.slice(0, 10)}</div>
-        <div className="tags">
-        <div className="tag">{review.category}</div>
-        </div>
-        </div>
-        </div>
-        </Link>
-        </div>
+    return  <li class="hex">
+            <Link to={`/reviews/${review.review_id}`}>
+            <div class="hexIn">
+            <div class="hexLink" href="#">
+            <div class='img' style={{'background-image': `url(${review.review_img_url})`}}></div>
+            <h1 id="demo1">{review.title}</h1>
+            <p id="demo2">{review.category}</p>
+            <p id="demo3">{review.created_at.slice(0, 10)}</p>
+            <p id="demo4">{review.owner}</p>
+            </div>
+            </div>
+            </Link>
+            </li>
 })}
+</ul>
+</div>
 </section>
 </div>
 );
